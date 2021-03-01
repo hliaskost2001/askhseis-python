63b248ab6326ff6b02d872f58a32a5134e8f645d
hliaskost2001 <76534565+hliaskost2001@users.noreply.github.com>
2021-03-01 11:36:08 +0200

    Add files via upload

---
+++ ask5.py
@@ -0,0 +1,15 @@
+import random
+count=1
+k=50
+mo=100
+ori_kath=['S','O','S']
+diag=['S','O','S']
+random.shuffle(ori_kath)
+for i in range(k):
+    print(ori_kath)
+else:
+    random.shuffle(diag)
+for i in range(k):
+     print(diag)
+average=mo/count
+print(average)

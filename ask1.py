f56a14cb9d3e3a9bc6a70788308696a21a021382
hliaskost2001 <76534565+hliaskost2001@users.noreply.github.com>
2021-03-02 20:55:47 +0200

    Update ask1.py

--- ask1.py
+++ ask1.py
@@ -1,18 +1,18 @@
 import random
 count=1
+#Οριζόντιες και κάθετες
 ori_kath= [0,1]
+#Διαγωνίους
 diagwnioi=[0,1]
 m=50
-#Οριζόντιες και κάθετες
-list1= ori_kath*5
-#Διαγωνιοι
-list2=diagwnioi*5
-random.shuffle(list1)
+o_k= ori_kath*5
+d=diagwnioi*5
+random.shuffle(o_k)
 for i in range(m):
-    print(list1)
+    print(o_k)
 else:
-    random.shuffle(list2)
+    random.shuffle(d)
 for j in range(m):
-    print(list2)
+    print(d)
 average=100/count
 print(average)

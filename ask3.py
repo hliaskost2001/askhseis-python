b0e26774f3540c36d416a0c284214c2c49b9656e
hliaskost2001 <76534565+hliaskost2001@users.noreply.github.com>
2021-03-02 22:26:34 +0200

    Update ask3.py

--- ask3.py
+++ ask3.py
@@ -1,14 +1,14 @@
 import urllib.request
 import json
 import  datetime
-d = datetime.datetime(2021, 1, 11)
+d = datetime.datetime(2021, 1, 30)
 print(d)
 
-url="https://api.opap.gr/draws/v3.0/1100/draw-date/2021-01-11/2021-01-11"
+url="https://api.opap.gr/draws/v3.0/1100/draw-date/2021-01-30/2021-01-30"
 r=urllib.request.urlopen(url)
 html=r.read()
 html=html.decode()
 data=json.loads(html,strict=False)
 for draw in data["content"]:
     t=draw["winningNumbers"]["list"]
-    print([min(t),max(t)])
+    print([max(t),min(t)])
